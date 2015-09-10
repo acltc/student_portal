5fbe1b27e6a3294ab64a864b11f0dc51fe5750c7
markeban <markeban@yahoo.com>
2015-09-10 16:39:53 -0300

    fixed mailer error

--- app/mailers/portal_mailer.rb
+++ app/mailers/portal_mailer.rb
@@ -2,7 +2,7 @@ class PortalMailer < ApplicationMailer
 
   def welcome_student(student)
     @student = student
-    mail(from: "no-reply@anyonecanlearntocode.com", to: @student.email, subject: 'Welcome to the Anyone Can Learn To Code Student Portal!', cc: "glenn@anyonecanlearntocode.com" bcc: "mark@anyonecanlearntocode.com")
+    mail(from: "no-reply@anyonecanlearntocode.com", to: @student.email, subject: 'Welcome to the Anyone Can Learn To Code Student Portal!', cc: "glenn@anyonecanlearntocode.com", bcc: "mark@anyonecanlearntocode.com")
   end
 
   def notify_student(student)
