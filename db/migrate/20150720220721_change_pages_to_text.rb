3e0ca37be34f6af807d3138e96a11bb6fb2e5ab7
Mark Richardson <mark@markeban.com>
2015-10-13 19:08:22 -0300

    fixed code syntax highlighting

--- db/migrate/20150720220721_change_pages_to_text.rb
+++
@@ -1,7 +0,0 @@
-class ChangePagesToText < ActiveRecord::Migration
-  def change
-    change_column :pages, :welcome_message,  :text
-    change_column :pages, :capstone,  :text
-    change_column :pages, :blog,  :text
-  end
-end
