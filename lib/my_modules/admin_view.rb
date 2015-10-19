67a7e4e6bce59bcf4a553f54035b8ce9e01b395e
markeban <markeban@yahoo.com>
2015-10-18 23:32:44 -0300

    'viewed by admin' wasn't working if last comment was by instructor, no query is on student_id, so works

--- lib/my_modules/admin_view.rb
+++ lib/my_modules/admin_view.rb
@@ -1,7 +1,7 @@
 module AdminView
 
   def reverse_admin_views(view_boolean)
-    assignments = self.class.where("user_id = ? AND assignment_id = ?", user_id, assignment_id)
+    assignments = self.class.where("student_id = ? AND assignment_id = ?", student_id, assignment_id)
     if assignments.update_all(viewed_by_admin: view_boolean)
       return assignments.last
     end
