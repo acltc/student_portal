5dd0558a4ab314c44030ebc15e262b25a780812c
Josh Nixon <seriousfools@gmail.com>
2016-04-08 15:02:48 -0500

    config static

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module StudentPortal
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += Dir["#{Rails.root}/lib/my_modules"]
+    config.serve_static_assets = true
   end
 end
