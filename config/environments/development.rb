0d68264ff95ab9cdbba837d3feffc1ce0b2afb0b
Mark Richardson <mark@markeban.com>
2017-02-06 09:19:15 +0200

    set domains for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Raises helpful error messages.
   # config.action_mailer.raise_delivery_errors = true
   # config.assets.raise_runtime_errors = true
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
     :user_name => ENV["SENDGRID_USERNAME"],
